c0b8cf336d5d8a9f58caa16f23f6e237a6494f47
Nathan Gaberel <nathang@theodo.co.uk>
2017-05-20 18:14:54 +0100

    Install and setup AOS.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import smoothScroll from 'smoothscroll-polyfill'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 import Layout from './Layout'
 
+AOS.init()
 smoothScroll.polyfill()
 
 ReactDOM.render(
